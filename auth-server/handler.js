fd8abaa8e06e5b54647dd1368cab57d4d293e68b
rrigs <rrachaelar@gmail.com>
2021-05-05 17:36:27 -0400

    edited handler.js

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -12,7 +12,7 @@ const credentials = {
   auth_uri: 'https://accounts.google.com/o/oauth2/auth',
   token_uri: 'https://oauth2.googleapis.com/token',
   auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-  redirect_uris: ['https://rrigs.github.io/meetv2/'],
+  redirect_uris: ['https://rrigs.github.io/meet-app/'],
   javascript_origins: [
       'https://rrigs.github.io',
       'http://localhost:3000',
